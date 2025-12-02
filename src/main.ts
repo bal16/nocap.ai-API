c21e1a22df8c30786cb377654457a7aa62dab74e
bal16 <ahmadhamidbalya@gmail.com>
2025-12-02 22:32:23 +0700

    docs: add logger usage example

--- src/main.ts
+++ src/main.ts
@@ -54,7 +54,8 @@ const app = new Elysia()
   )
   .get(
     '/',
-    () => {
+    (ctx) => {
+      ctx.log.info('Root endpoint accessed');
       return {
         message: 'Hello Elysia',
       };
