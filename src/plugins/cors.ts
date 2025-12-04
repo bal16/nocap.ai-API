3e6d7eba2c094e135bcd5948917fe67bb0b684cf
bal16 <ahmadhamidbalya@gmail.com>
2025-12-04 23:48:32 +0700

    refactor(cors): separate to plugins dir

---
+++ src/plugins/cors.ts
@@ -0,0 +1,10 @@
+import { Elysia } from 'elysia';
+import cors from '@elysiajs/cors';
+
+import { env } from '../shared/env';
+
+export const corsPlugins = new Elysia({ name: 'cors' }).use(
+  cors({
+    origin: env.ORIGIN,
+  })
+);
