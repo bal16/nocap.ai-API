115da8fd4c9da31be37c4a40df77a59a16d5e9c1
bal16 <ahmadhamidbalya@gmail.com>
2025-12-06 21:45:51 +0700

    fix: update logger configuration to use environment variables

--- src/plugins/logger.ts
+++ src/plugins/logger.ts
@@ -1,19 +1,22 @@
 import { wrap } from '@bogeychan/elysia-logger';
 import { Elysia } from 'elysia';
 import pino from 'pino';
+import { env } from '../shared/env';
+
+const isDev = env.NODE_ENV === 'development';
 
 export const logger = pino({
-  level: 'debug',
-  transport: {
-    target: 'pino-pretty', // For human-readable console output
-    options: {
-      colorize: true,
-    },
-  },
+  level: env.LOG_LEVEL,
+  ...(isDev
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: { colorize: true },
+        },
+      }
+    : {}), // no pretty in production
 });
 
 export const loggerPlugins = new Elysia({ name: 'logger' }).use(
-  wrap(logger, {
-    autoLogging: true,
-  })
+  wrap(logger, { autoLogging: true })
 );
